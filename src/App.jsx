e0e81d982cbb219e195756db3f23d2ea6ba64f41
IsacHolstad <wisac22@gmail.com>
2023-09-07 13:57:14 +0200

    new tailwindcss prefixer fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2 className="bg-red-400 text-red text-center">helo</h2>
+      <h2 className="bg-red-400 text-red text-center text-2xl">hello</h2>
     </>
   )
 }

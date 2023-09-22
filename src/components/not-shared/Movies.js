5609df65148e37517506b573b26519f960753fa3
IsacHolstad <wisac22@gmail.com>
2023-09-22 13:52:40 +0200

    wording change

--- src/components/not-shared/Movies.js
+++ src/components/not-shared/Movies.js
@@ -18,7 +18,7 @@ const Movies = () => {
                         <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
                             <img src={file.source} alt="" className="pointer-events-none object-cover group-hover:opacity-75" />
                             <button type="button" className="absolute inset-0 focus:outline-none">
-                                <span className="sr-only">View details for {file.title}</span>
+                                <span className="sr-only">View details {file.title}</span>
                             </button>
                         </div>
                         <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">{file.title}</p>

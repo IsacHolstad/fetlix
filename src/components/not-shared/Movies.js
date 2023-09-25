b66daf90121962487b0bd45ffd55a0932c29fa3a
IsacHolstad <wisac22@gmail.com>
2023-09-25 10:15:50 +0200

    added more movies

--- src/components/not-shared/Movies.js
+++ src/components/not-shared/Movies.js
@@ -6,7 +6,16 @@ const files = [
         source:
             'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
     },
-    // More files...
+    {
+        title: 'Wolf of wall-street',
+        source:
+            'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80'
+    },
+    {
+        title: 'Fightclub',
+        source:
+            'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80'
+    },
 ]
 
 const Movies = () => {
